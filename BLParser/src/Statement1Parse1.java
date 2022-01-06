58dca8d285154ba60683866148bc9b75bd5bb519
Connor Michetti <89315956+cmichetti95@users.noreply.github.com>
2022-01-05 21:06:15 -0500

    Add files via upload

---
+++ BLParser/src/Statement1Parse1.java
@@ -0,0 +1,261 @@
+import components.queue.Queue;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+import components.statement.Statement;
+import components.statement.Statement1;
+import components.utilities.Reporter;
+import components.utilities.Tokenizer;
+
+/**
+ * Layered implementation of secondary methods {@code parse} and
+ * {@code parseBlock} for {@code Statement}.
+ *
+ * @author Connor Michetti and Nick Folino
+ *
+ */
+public final class Statement1Parse1 extends Statement1 {
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+
+    /**
+     * Converts {@code c} into the corresponding {@code Condition}.
+     *
+     * @param c
+     *            the condition to convert
+     * @return the {@code Condition} corresponding to {@code c}
+     * @requires [c is a condition string]
+     * @ensures parseCondition = [Condition corresponding to c]
+     */
+    private static Condition parseCondition(String c) {
+        assert c != null : "Violation of: c is not null";
+        assert Tokenizer
+                .isCondition(c) : "Violation of: c is a condition string";
+        return Condition.valueOf(c.replace('-', '_').toUpperCase());
+    }
+
+    /**
+     * Parses an IF or IF_ELSE statement from {@code tokens} into {@code s}.
+     *
+     * @param tokens
+     *            the input tokens
+     * @param s
+     *            the parsed statement
+     * @replaces s
+     * @updates tokens
+     * @requires <pre>
+     * [<"IF"> is a prefix of tokens]  and
+     *  [<Tokenizer.END_OF_INPUT> is a suffix of tokens]
+     * </pre>
+     * @ensures <pre>
+     * if [an if string is a proper prefix of #tokens] then
+     *  s = [IF or IF_ELSE Statement corresponding to if string at start of #tokens]  and
+     *  #tokens = [if string at start of #tokens] * tokens
+     * else
+     *  [reports an appropriate error message to the console and terminates client]
+     * </pre>
+     */
+    private static void parseIf(Queue<String> tokens, Statement s) {
+        assert tokens != null : "Violation of: tokens is not null";
+        assert s != null : "Violation of: s is not null";
+        assert tokens.length() > 0 && tokens.front().equals("IF") : ""
+                + "Violation of: <\"IF\"> is proper prefix of tokens";
+
+        //consume IF
+        tokens.dequeue();
+        String condition = tokens.dequeue();
+
+        //check condition
+        Reporter.assertElseFatalError(Tokenizer.isCondition(condition),
+                "Invalid syntax. Expected BL condition");
+        Condition cond = parseCondition(condition);
+
+        //check and consume THEN
+        Reporter.assertElseFatalError(tokens.dequeue().equals("THEN"),
+                "Invalid syntax. Expected THEN");
+        s.parseBlock(tokens);
+
+        Statement ifBlock = s.newInstance();
+        ifBlock.transferFrom(s);
+
+        //determine if or if else
+        if (tokens.front().equals("ELSE")) {
+            tokens.dequeue();
+            Statement elseBlock = s.newInstance();
+            s.parseBlock(tokens);
+            elseBlock.transferFrom(s);
+            s.assembleIfElse(cond, ifBlock, elseBlock);
+            Reporter.assertElseFatalError(tokens.dequeue().equals("END"),
+                    "Invalid syntax. Expected END");
+            Reporter.assertElseFatalError(tokens.dequeue().equals("IF"),
+                    "Invalid syntax. Expected IF");
+        } else {
+            Reporter.assertElseFatalError(tokens.dequeue().equals("END"),
+                    "Invalid syntax. Expected END");
+            Reporter.assertElseFatalError(tokens.dequeue().equals("IF"),
+                    "Invalid syntax. Expected IF");
+            s.assembleIf(cond, ifBlock);
+        }
+
+    }
+
+    /**
+     * Parses a WHILE statement from {@code tokens} into {@code s}.
+     *
+     * @param tokens
+     *            the input tokens
+     * @param s
+     *            the parsed statement
+     * @replaces s
+     * @updates tokens
+     * @requires <pre>
+     * [<"WHILE"> is a prefix of tokens]  and
+     *  [<Tokenizer.END_OF_INPUT> is a suffix of tokens]
+     * </pre>
+     * @ensures <pre>
+     * if [a while string is a proper prefix of #tokens] then
+     *  s = [WHILE Statement corresponding to while string at start of #tokens]  and
+     *  #tokens = [while string at start of #tokens] * tokens
+     * else
+     *  [reports an appropriate error message to the console and terminates client]
+     * </pre>
+     */
+    private static void parseWhile(Queue<String> tokens, Statement s) {
+        assert tokens != null : "Violation of: tokens is not null";
+        assert s != null : "Violation of: s is not null";
+        assert tokens.length() > 0 && tokens.front().equals("WHILE") : ""
+                + "Violation of: <\"WHILE\"> is proper prefix of tokens";
+
+        tokens.dequeue();
+        String condS = tokens.dequeue();
+        Reporter.assertElseFatalError(Tokenizer.isCondition(condS),
+                "Invalid syntax. Expected BL condition");
+        Condition condition = parseCondition(condS);
+        Reporter.assertElseFatalError(tokens.dequeue().equals("DO"),
+                "Invalid syntax. Expected DO");
+        s.parseBlock(tokens);
+        Reporter.assertElseFatalError(tokens.dequeue().equals("END"),
+                "Invalid syntax. Expected END");
+        Reporter.assertElseFatalError(tokens.dequeue().equals("WHILE"),
+                "Invalid syntax. Expected WHILE");
+
+        Statement whileBlock = s.newInstance();
+        whileBlock.transferFrom(s);
+        s.assembleWhile(condition, whileBlock);
+
+    }
+
+    /**
+     * Parses a CALL statement from {@code tokens} into {@code s}.
+     *
+     * @param tokens
+     *            the input tokens
+     * @param s
+     *            the parsed statement
+     * @replaces s
+     * @updates tokens
+     * @requires [identifier string is a proper prefix of tokens]
+     * @ensures <pre>
+     * s =
+     *   [CALL Statement corresponding to identifier string at start of #tokens]  and
+     *  #tokens = [identifier string at start of #tokens] * tokens
+     * </pre>
+     */
+    private static void parseCall(Queue<String> tokens, Statement s) {
+        assert tokens != null : "Violation of: tokens is not null";
+        assert s != null : "Violation of: s is not null";
+        assert tokens.length() > 0
+                && Tokenizer.isIdentifier(tokens.front()) : ""
+                        + "Violation of: identifier string is proper prefix of tokens";
+
+        s.assembleCall(tokens.dequeue());
+    }
+
+    /*
+     * Constructors -----------------------------------------------------------
+     */
+
+    /**
+     * No-argument constructor.
+     */
+    public Statement1Parse1() {
+        super();
+    }
+
+    /*
+     * Public methods ---------------------------------------------------------
+     */
+
+    @Override
+    public void parse(Queue<String> tokens) {
+        assert tokens != null : "Violation of: tokens is not null";
+        assert tokens.length() > 0 : ""
+                + "Violation of: Tokenizer.END_OF_INPUT is a suffix of tokens";
+
+        if (tokens.front().equals("IF")) {
+            parseIf(tokens, this);
+        } else if (tokens.front().equals("WHILE")) {
+            parseWhile(tokens, this);
+        } else {
+            parseCall(tokens, this);
+        }
+
+    }
+
+    @Override
+    public void parseBlock(Queue<String> tokens) {
+        assert tokens != null : "Violation of: tokens is not null";
+        assert tokens.length() > 0 : ""
+                + "Violation of: Tokenizer.END_OF_INPUT is a suffix of tokens";
+
+        Statement nextS = this.newInstance();
+        while ((!tokens.front().equals("ELSE") && !tokens.front().equals("END")
+                && !tokens.front().equals(Tokenizer.END_OF_INPUT))) {
+            nextS.parse(tokens);
+            this.addToBlock(this.lengthOfBlock(), nextS);
+            nextS.clear();
+        }
+
+    }
+
+    /*
+     * Main test method -------------------------------------------------------
+     */
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        /*
+         * Get input file name
+         */
+        out.print("Enter valid BL statement(s) file name: ");
+        String fileName = in.nextLine();
+        /*
+         * Parse input file
+         */
+        out.println("*** Parsing input file ***");
+        Statement s = new Statement1Parse1();
+        SimpleReader file = new SimpleReader1L(fileName);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        s.parse(tokens); // replace with parseBlock to test other method
+        /*
+         * Pretty print the statement(s)
+         */
+        out.println("*** Pretty print of parsed statement(s) ***");
+        s.prettyPrint(out, 0);
+
+        in.close();
+        out.close();
+    }
+
+}
---
+++ BLParser/test/StatementTest.java
@@ -0,0 +1,371 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import components.queue.Queue;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.statement.Statement;
+import components.utilities.Tokenizer;
+
+/**
+ * JUnit test fixture for {@code Statement}'s constructor and kernel methods.
+ *
+ * @author Put your name here
+ *
+ */
+public abstract class StatementTest {
+
+    /**
+     * The name of a file containing a sequence of BL statements.
+     */
+    private static final String FILE_NAME_1 = "test/statement1.bl",
+            FILE_NAME_2 = "test/statement2.bl",
+            FILE_NAME_3 = "test/statement3.bl",
+            FILE_NAME_4 = "test/statement4.bl",
+            FILE_NAME_5 = "test/statement5.bl",
+            FILE_NAME_6 = "test/statement6.bl",
+            FILE_NAME_7 = "test/statement7.bl",
+            FILE_NAME_8 = "test/statement8.bl";
+
+    /**
+     * Invokes the {@code Statement} constructor for the implementation under
+     * test and returns the result.
+     *
+     * @return the new statement
+     * @ensures constructorTest = compose((BLOCK, ?, ?), <>)
+     */
+    protected abstract Statement constructorTest();
+
+    /**
+     * Invokes the {@code Statement} constructor for the reference
+     * implementation and returns the result.
+     *
+     * @return the new statement
+     * @ensures constructorRef = compose((BLOCK, ?, ?), <>)
+     */
+    protected abstract Statement constructorRef();
+
+    /**
+     * Test of parse on syntactically valid input.
+     */
+    @Test
+    public final void testParseValidExample() {
+        /*
+         * Setup
+         */
+        Statement sRef = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_1);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        sRef.parse(tokens);
+        file.close();
+        Statement sTest = this.constructorTest();
+        file = new SimpleReader1L(FILE_NAME_1);
+        tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call
+         */
+        sTest.parse(tokens);
+        /*
+         * Evaluation
+         */
+        assertEquals(sRef, sTest);
+    }
+
+    /**
+     * Test of parse on syntactically invalid input.
+     */
+    @Test(expected = RuntimeException.class)
+    public final void testParseErrorExample() {
+        /*
+         * Setup
+         */
+        Statement sTest = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_2);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call--should result in an error being caught
+         */
+        sTest.parse(tokens);
+    }
+
+    // TODO - add more test cases for valid inputs for both parse and parseBlock
+
+    /**
+     * Test of parseBlock on syntactically valid input.
+     */
+    @Test
+    public final void testParseValidFile3() {
+        /*
+         * Setup
+         */
+        Statement sRef = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_3);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        sRef.parseBlock(tokens);
+        file.close();
+        Statement sTest = this.constructorTest();
+        file = new SimpleReader1L(FILE_NAME_3);
+        tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call
+         */
+        sTest.parseBlock(tokens);
+        /*
+         * Evaluation
+         */
+        assertEquals(sRef, sTest);
+    }
+
+    /**
+     * Test of parseBlock on syntactically valid input.
+     */
+    @Test
+    public final void testParseBlockValidFile3() {
+        /*
+         * Setup
+         */
+        Statement sRef = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_3);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        sRef.parseBlock(tokens);
+        file.close();
+        Statement sTest = this.constructorTest();
+        file = new SimpleReader1L(FILE_NAME_3);
+        tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call
+         */
+        sTest.parseBlock(tokens);
+        /*
+         * Evaluation
+         */
+        assertEquals(sRef, sTest);
+    }
+
+    /**
+     * Test of parse on syntactically valid input.
+     */
+    @Test
+    public final void testParseValidFile4() {
+        /*
+         * Setup
+         */
+        Statement sRef = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_4);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        sRef.parse(tokens);
+        file.close();
+        Statement sTest = this.constructorTest();
+        file = new SimpleReader1L(FILE_NAME_4);
+        tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call
+         */
+        sTest.parse(tokens);
+        /*
+         * Evaluation
+         */
+        assertEquals(sRef, sTest);
+    }
+
+    /**
+     * Test of parseBlock on syntactically valid input.
+     */
+    @Test
+    public final void testParseBlockValidFile4() {
+        /*
+         * Setup
+         */
+        Statement sRef = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_4);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        sRef.parseBlock(tokens);
+        file.close();
+        Statement sTest = this.constructorTest();
+        file = new SimpleReader1L(FILE_NAME_4);
+        tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call
+         */
+        sTest.parseBlock(tokens);
+        /*
+         * Evaluation
+         */
+        assertEquals(sRef, sTest);
+    }
+
+    /**
+     * Test of parse on syntactically valid input.
+     */
+    @Test
+    public final void testParseValidFile5() {
+        /*
+         * Setup
+         */
+        Statement sRef = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_5);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        sRef.parse(tokens);
+        file.close();
+        Statement sTest = this.constructorTest();
+        file = new SimpleReader1L(FILE_NAME_5);
+        tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call
+         */
+        sTest.parse(tokens);
+        /*
+         * Evaluation
+         */
+        assertEquals(sRef, sTest);
+    }
+
+    /**
+     * Test of parseBlock on syntactically valid input.
+     */
+    @Test
+    public final void testParseBlockValidFile5() {
+        /*
+         * Setup
+         */
+        Statement sRef = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_5);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        sRef.parseBlock(tokens);
+        file.close();
+        Statement sTest = this.constructorTest();
+        file = new SimpleReader1L(FILE_NAME_5);
+        tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call
+         */
+        sTest.parseBlock(tokens);
+        /*
+         * Evaluation
+         */
+        assertEquals(sRef, sTest);
+    }
+
+    // TODO - add more test cases for as many distinct syntax errors as possible
+    //        for both parse and parseBlock
+
+    /**
+     * Test of parse on syntactically invalid input.
+     */
+    @Test(expected = RuntimeException.class)
+    public final void testParseInvalidIfThenFile6() {
+        /*
+         * Setup
+         */
+        Statement sTest = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_6);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call--should result in an error being caught
+         */
+        sTest.parse(tokens);
+    }
+
+    /**
+     * Test of parseBlock on syntactically invalid input. Incorrect first
+     * IF-THEN
+     */
+    @Test(expected = RuntimeException.class)
+    public final void testParseBlockInvalidIfThenFile6() {
+        /*
+         * Setup
+         */
+        Statement sTest = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_6);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call--should result in an error being caught
+         */
+        sTest.parseBlock(tokens);
+    }
+
+    /**
+     * Test of parse on syntactically invalid input, words after the end.
+     */
+    @Test(expected = RuntimeException.class)
+    public final void testParseInvalidEndFile7() {
+        /*
+         * Setup
+         */
+        Statement sTest = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_7);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call--should result in an error being caught
+         */
+        sTest.parse(tokens);
+    }
+
+    /**
+     * Test of parseBlock on syntactically invalid input. Incorrect first
+     * IF-THEN
+     */
+    @Test(expected = RuntimeException.class)
+    public final void testParseBlockInvalidEndFile7() {
+        /*
+         * Setup
+         */
+        Statement sTest = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_7);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call--should result in an error being caught
+         */
+        sTest.parseBlock(tokens);
+    }
+
+    /**
+     * Test of parse on syntactically invalid input. Incorrect while condition.
+     */
+    @Test(expected = RuntimeException.class)
+    public final void testParseInvalidWhileCondFile8() {
+        /*
+         * Setup
+         */
+        Statement sTest = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_8);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call--should result in an error being caught
+         */
+        sTest.parse(tokens);
+    }
+
+    /**
+     * Test of parseBlock on syntactically invalid input. Incorrect while
+     * condition.
+     */
+    @Test(expected = RuntimeException.class)
+    public final void testParseBlockInvalidWhileCondFile8() {
+        /*
+         * Setup
+         */
+        Statement sTest = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(FILE_NAME_8);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        /*
+         * The call--should result in an error being caught
+         */
+        sTest.parseBlock(tokens);
+    }
+}
